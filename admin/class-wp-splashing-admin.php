fa6ac74d479fcb796c8732c902ea08c786275186
Jan Henckens <desk@janhenckens.com>
2016-08-20 20:21:12 +0200

    Added unsplash license information metabox

--- admin/class-wp-splashing-admin.php
+++ admin/class-wp-splashing-admin.php
@@ -81,7 +81,17 @@ class Wp_Splashing_Admin {
 					<div id="post-body" class="metabox-holder columns-2">
 						<div class="metabox-holder columns-2">
 							<div id="post-body-content" style="position: relative;" class="postbox-container"></div>
-							<div id="postbox-container-1" class="postbox-container"></div>
+							<div id="postbox-container-1" class="postbox-container">
+								<div class="postbox">
+									<h2 class="hndle"><?php _e('Powered by Unsplash', 'wp-splashing'); ?></h2>
+									<div class="inside">
+										<p><?php _e('Splashing Images is powered by <a href="http://unsplash.com">unsplash.com</a> and the Unsplash API.', 'wp-splashing'); ?></p>
+										<h3><?php _e('Unsplash License ', 'wp-splashing'); ?></h3>
+										<p><?php _e('All photos published on Unsplash are licensed under Creative Commons Zero which means you can copy, modify, distribute and use the photos for free, including commercial purposes, without asking permission from or providing attribution to the photographer or Unsplash.', 'wp-splashing'); ?></p>
+
+									</<div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
