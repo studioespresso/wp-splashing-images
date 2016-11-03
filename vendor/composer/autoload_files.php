06596361808ddd29b28feaebd603e4f61962d159
Jan Henckens <jan@statik.be>
2016-11-03 19:02:57 +0100

    Removed mmucklo/krumo for production release

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -9,5 +9,4 @@ return array(
     'a0edc8309cc5e1d60e3047b5df6b7052' => $vendorDir . '/guzzlehttp/psr7/src/functions_include.php',
     'c964ee0ededf28c96ebd9db5099ef910' => $vendorDir . '/guzzlehttp/promises/src/functions_include.php',
     '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
-    'efd893c01f001f6c8c594ea00713c08c' => $vendorDir . '/mmucklo/krumo/class.krumo.php',
 );
