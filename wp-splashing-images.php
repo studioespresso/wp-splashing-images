fd67e952c58f52495519f186f97b1816ba42b6cb
Jan Henckens <jan@studioespresso.co>
2016-11-04 16:45:41 +0100

    Short description update

--- wp-splashing-images.php
+++ wp-splashing-images.php
@@ -15,7 +15,7 @@
  * @wordpress-plugin
  * Plugin Name:       Splashing Images
  * Plugin URI:        http://studioespresso.co
- * Description:       Quick access to amazing photos from unsplash.com, add photos with one click and use them in your content right away.
+ * Description:       Unsplash.com photos, right in your dashboard. Add photos with one click and use them in your content right away.
  * Version:           1.0.0
  * Author:            Studio Espresso
  * Author URI:        http://studioespresso.co
