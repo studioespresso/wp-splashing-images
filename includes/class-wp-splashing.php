fd7fb2b72965947e69c938825e3d88f8828cb7cf
Jan Henckens <jan@studioespresso.co>
2016-11-05 21:14:52 +0100

    Removed public classes en action since the plugin only works in the dashboard

--- includes/class-wp-splashing.php
+++ includes/class-wp-splashing.php
@@ -74,7 +74,6 @@ class Wp_Splashing {
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
-		$this->define_public_hooks();
 
 	}
 
@@ -117,12 +116,6 @@ class Wp_Splashing {
 		 * The class responsible for defining all actions unsplash related function.
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-wp-splashing-unsplash.php';
-
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-wp-splashing-public.php';
 
         require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wp-splashing-extra.php';
 
@@ -172,21 +165,6 @@ class Wp_Splashing {
 
 	}
 
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks() {
-
-		$plugin_public = new Wp_Splashing_Public( $this->get_plugin_name(), $this->get_version() );
-
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
-
-	}
 
 	/**
 	 * Run the loader to execute all of the hooks with WordPress.
