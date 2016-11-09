9f88f102f693aac6b8cf2e199bf633b570928a0b
Jan Henckens <jan@statik.be>
2016-11-09 17:54:57 +0100

    #6 Refactor loading

--- admin/js/wp-splashing-admin.js
+++ admin/js/wp-splashing-admin.js
@@ -31,11 +31,9 @@ jQuery(document).ready(function($) {
                     nonce: settings.wp_splashing_admin_nonce,
                 },
                 beforeSend: function() {
-                    console.log('Submitting image for download');
                     image.LoadingOverlay("show");
                 },
-                success: function(response) {                                               
-                    console.log(response); 
+                success: function(response) {
                     image.LoadingOverlay("hide");
                     var checkmark = '<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>';
                     element.append(checkmark);
