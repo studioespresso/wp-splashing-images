d6832af86b242c8cd0ef708056446d0705c27ffe
Jan Henckens <jan@studioespresso.co>
2017-06-05 10:37:22 +0200

    Use tabs instead of buttons

--- admin/partials/wp-splashing-admin-main.php
+++ admin/partials/wp-splashing-admin-main.php
@@ -32,33 +32,20 @@
 			</div>
 <?php } ?>
 
+	<h2 class="nav-tab-wrapper">
+
+		<a href="/wp-admin/upload.php?page=wp-splashing&mode=popular" class="nav-tab <?php echo $_GET['mode'] == 'popular' ? 'nav-tab-active' : ''; ?>" alt="<?php _e('25 most popular photos on Unsplash', 'wp-splashing-images'); ?>"><span style="color: green;">&#8599; </span><?php _e('Popular', 'wp-splashing-images'); ?></a>
+		<a href="/wp-admin/upload.php?page=wp-splashing&mode=latest" class="nav-tab <?php echo $_GET['mode'] == 'latest' ? 'nav-tab-active' : ''; ?>" alt="<?php _e('Shows the 25 last added images', 'wp-splashing-images'); ?>"><?php _e('&#128349; Last added', 'wp-splashing-images'); ?></a>
+		<a href="/wp-admin/upload.php?page=wp-splashing&mode=random" class="nav-tab <?php echo $_GET['mode'] == 'random' ? 'nav-tab-active' : ''; ?>" alt="<?php _e('Shows 25 random images', 'wp-splashing-images'); ?>"><span style="color: gold;">&#9786; </span><?php _e('Surprise me :)', 'wp-splashing-images'); ?></a>
+		<?php if($this->unsplash->isUnsplashUser()) { ?>
+		<a href="/wp-admin/upload.php?page=wp-splashing&mode=liked" class="nav-tab <?php echo $_GET['mode'] == 'liked' ? 'nav-tab-active' : ''; ?>" alt="<?php _e('Shows 25 random images', 'wp-splashing-images'); ?>"><span style="color: red;">&hearts; </span><?php _e('Photos I like', 'wp-splashing-images'); ?></a>
+		<a href="/wp-admin/upload.php?page=wp-splashing&mode=mine" class="nav-tab <?php echo $_GET['mode'] == 'mine' ? 'nav-tab-active' : ''; ?>" alt="<?php _e('Shows 25 random images', 'wp-splashing-images'); ?>"><span style="color: gold;">&#128100; </span><?php _e('My photos', 'wp-splashing-images'); ?></a>
+		<?php } ?>
+
+	</h2>
 	<div id="poststuff">
         <div id="post-body" class="metabox-holder columns-2">
 			<div id="wp-splashing_images" style="position: relative;" class="postbox-container">
-				<div class="media-toolbar wp-filter">
-					<div class="media-toolbar-primary">
-					</div>
-                    <div class="media-toolbar-primary">
-                        <a href="/wp-admin/upload.php?page=wp-splashing&mode=popular" class="button btn--inline" alt="<?php _e('25 most popular photos on Unsplash', 'wp-splashing-images'); ?>"><span style="color: green;">&#8599; </span><?php _e('Popular', 'wp-splashing-images'); ?></a>
-                        <a href="/wp-admin/upload.php?page=wp-splashing&mode=latest" class="button btn--inline" alt="<?php _e('Shows the 25 last added images', 'wp-splashing-images'); ?>"><?php _e('&#128349; Last added', 'wp-splashing-images'); ?></a>
-                        <a href="/wp-admin/upload.php?page=wp-splashing&mode=random" class="button btn--inline" alt="<?php _e('Shows 25 random images', 'wp-splashing-images'); ?>"><span style="color: gold;">&#9786; </span><?php _e('Surprise me :)', 'wp-splashing-images'); ?></a>
-						<?php if($this->unsplash->isUnsplashUser()) { ?>
-						<a href="/wp-admin/upload.php?page=wp-splashing&mode=liked" class="button btn--inline" alt="<?php _e('Shows 25 random images', 'wp-splashing-images'); ?>"><span style="color: red;">&hearts; </span><?php _e('Photos I like', 'wp-splashing-images'); ?></a>
-							<a href="/wp-admin/upload.php?page=wp-splashing&mode=mine" class="button btn--inline" alt="<?php _e('Shows 25 random images', 'wp-splashing-images'); ?>"><span style="color: gold;">&#128100; </span><?php _e('My photos', 'wp-splashing-images'); ?></a>
-							<?php } ?>
-                    </div>
-                    <div class="media-toolbar-secondary">
-
-                        <form id="splashing-search" method="get" action="<?php echo esc_url( admin_url('admin-post.php') ); ?>">
-						<label class="screen-reader-text" for="post-search-input">Search Posts:</label>
-						<input type="search" id="post-search-input" name="search" value="<?php echo $_GET['search']; ?>">
-						<input type="hidden" name="action" value="wp_splashing_search">
-						<input type="hidden" name="paged" value="1">
-						<input type="hidden" name="nonce" value="<?php echo wp_create_nonce('wp_splashing_nonce'); ?>">
-						<input type="submit" id="search-submit" class="button" value="<?php _e('&#128269; Search Unsplash','wp-splashing-images');?>">
-					</form>
-					</div>
-				</div>
 				<div id="splashing-images">
 					<?php
 					if(isset($_GET['search'])) {
